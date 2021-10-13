3c257f83151395adac5ad34d407669c436f4fb20
zaki0245 <muhammad.zaki0245@gmail.com>
2021-10-13 14:13:17 +0700

    tambah kolom dan menu

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -22,6 +22,7 @@
                                 <th>NIM</th>
                                 <th>Name</th>
                                 <th>Class</th>
+                                <th>Department</th>
                                 <th>Action</th>
                             </tr>
                         </thead>
@@ -31,6 +32,7 @@
                                 <td>{{ $s->nim }}</td>
                                 <td>{{ $s->name }}</td>
                                 <td>{{ $s->class }}</td>
+                                <td>{{ $s->department}}</td>
                                 <td>
                                 <form action="/students/{{$s->id}}" method="post">
                                 <a href="/students/{{$s->id}}/edit" class="btn btn-warning">Edit</a>
